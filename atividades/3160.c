2af4c7a43287f0cc23a9c2629421b77db06e7587
João Vitor Mancio Chaves <80546427+JoaoVitorChaves-05@users.noreply.github.com>
2024-11-12 01:17:52 +0000

    file: 3160.c

--- atividades/3160.c
+++ atividades/3160.c
@@ -1,77 +1,107 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+struct Friend {
+    char name[50];
+    struct Friend *next;
+};
+
+void push(char new_friend[50], struct Friend **head_ref) {
+
+    struct Friend *new_f = malloc(sizeof(struct Friend));
+    strcpy(new_f->name, new_friend);
+    new_f->next = NULL;
+
+    
+    if (*head_ref == NULL) {
+        *head_ref = new_f;
+    } else {
+
+        struct Friend *pointer_new_friend = *head_ref;
+        while (pointer_new_friend->next != NULL) {
+            pointer_new_friend = pointer_new_friend->next;
+        }
+        
+        pointer_new_friend->next = new_f;
+    }
+
+}
+
+struct Friend* find(char friend_to_find[50], struct Friend *head_ref) {
+    struct Friend *current = head_ref;
+    while (current->next != NULL) {
+        if (strcmp(current->next->name, friend_to_find) == 0) {
+            return current;
+        } else if (strcmp(current->name, friend_to_find) == 0) {
+            struct Friend *new_f = malloc(sizeof(struct Friend));
+            strcpy(new_f->name, "None");
+            new_f->next = current;
+            return new_f;
+        }
+
+        current = current->next;
+    }
+
+    return NULL;
+}
+
 int main() {
-    char L[300];
+    char L[500];
     char N[500];
     char S[50];
-    char tempN[500];
-    char originalL[300];
 
     gets(L);
     gets(N);
     gets(S);
 
-    strcpy(originalL, L);
-
-    if (strcmp(S, "nao") == 0) {
-
-        char *names1 = strtok(L, " ");
-
-        while (names1 != NULL) {
-            printf("%s ", names1);
-            names1 = strtok(NULL, " ");
-        }
-
-        char *names2 = strtok(N, " ");
-
-        while (names2 != NULL) {
-            printf("%s ", names2);
-            names2 = strtok(NULL, " ");
-        }
-
-        printf("\n");
-        return 0;
+    struct Friend *head_friend_list = NULL;
+    char *token = strtok(L, " ");
+    while (token != NULL) {
+        push(token, &head_friend_list);
+        token = strtok(NULL, " ");
     }
 
-    char *names = strtok(L, " ");
+    struct Friend *head_new_friends = NULL;
+    token = strtok(N, " ");
+    while (token != NULL) {
+        push(token, &head_new_friends);
+        token = strtok(NULL, " ");
+    }
+    
+    if (strcmp(S, "nao") != 0) {
+        struct Friend *current = find(S, head_friend_list);
 
-    int count = 0;
+        if (current != NULL) {
+            struct Friend *next_of_current = current->next;
+            current->next = head_new_friends;
 
-    while (names != NULL) {
-
-        if (strcmp(S, names) == 0) {
-
-            strcpy(tempN, N);
-            
-            char *newNames = strtok(tempN, " ");
-            while (newNames != NULL) {
-                printf(" ");
-                printf("%s", newNames);
-                newNames = strtok(NULL, " ");
+            if (strcmp(current->name, "None") == 0) {
+                head_friend_list = current->next;
             }
+
+            while (current->next != NULL) {
+                current = current->next;
+            }
+
+            current->next = next_of_current;
         }
-
-        printf("%s ", names);
-        count++;
-
-        names = strtok(NULL, " ");
+    } else {
+        struct Friend *current = head_friend_list;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = head_new_friends;
     }
 
-
-    char *completeNames = strtok(originalL, " ");
-
-    int count2 = 0;
-    while (completeNames != NULL) {
-        if (count == count2) {
-            printf("%s ", completeNames);
+    struct Friend *current = head_friend_list;
+    while (current != NULL) {
+        printf("%s", current->name);
+        if (current->next != NULL) {
+            printf(" ");
         }
-
-        completeNames = strtok(NULL, " ");
-        count2++;
+        current = current->next;
     }
 
     printf("\n");
-
-    return 0;
 }
