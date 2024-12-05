b04b61f9e7e5949d8e97aaf3d4e62d42ef27f743
João Vitor Mancio Chaves <80546427+JoaoVitorChaves-05@users.noreply.github.com>
2024-12-05 01:53:50 +0000

    feat: insertion e merge sort

--- atividades/insertion_e_merge_sort.c
+++ atividades/insertion_e_merge_sort.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-const int size = 10000;
-
 void insertionSort(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
 
@@ -14,37 +12,32 @@ void insertionSort(int arr[], int n) {
             j--;
         }
 
-        arr[j+1] = key;
+        arr[j + 1] = key;
     }
 }
 
-// Função recursiva para dividir e ordenar
+void mergeSort(int arr[], int left, int right);
+void merge(int arr[], int left, int mid, int right);
+
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
-        int mid = left + (right - left) / 2; // Encontra o meio
-
-        mergeSort(arr, left, mid);        // Ordena a primeira metade
-        mergeSort(arr, mid + 1, right);  // Ordena a segunda metade
-
-        merge(arr, left, mid, right);    // Mescla as duas metades
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
 void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1; // Tamanho do primeiro subarray
-    int n2 = right - mid;    // Tamanho do segundo subarray
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-    int L[n1], R[n2]; // Arrays temporários
+    int *L = malloc(n1 * sizeof(int));
+    int *R = malloc(n2 * sizeof(int));
 
-    // Copia os dados para os arrays temporários
-    for (int i = 0; i < n1; i++) {
-        L[i] = arr[left + i];
-    }
-    for (int j = 0; j < n2; j++) {
-        R[j] = arr[mid + 1 + j];
-    }
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
-    // Mescla os arrays temporários de volta em arr[]
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
@@ -57,67 +50,52 @@ void merge(int arr[], int left, int mid, int right) {
         k++;
     }
 
-    // Copia os elementos restantes de L[], se houver
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 
-    // Copia os elementos restantes de R[], se houver
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n");
+    free(L);
+    free(R);
 }
 
 int main() {
-    for (int nExecucoes = 0; nExecucoes < 10; nExecucoes++) {
-        int arr1[size];
-        int arr2[size];
-        clock_t t;
-        clock_t t2;
+    for (int size = 50000; size <= 500000; size += 50000) {
+        printf("\nPara N = %d:\n", size);
+        double insertionSortTime = 0;
+        double mergeSortTime = 0;
 
-        srand((unsigned)time(NULL));
+        for (int nExecucoes = 0; nExecucoes < 1; nExecucoes++) {
+            int *arr1 = malloc(size * sizeof(int));
+            int *arr2 = malloc(size * sizeof(int));
+            srand(time(NULL));
 
-        for (int i = 0; i < size; i++) {
-            int randomNumber = random() % 10001;
-            arr1[i] = randomNumber;
-            arr2[i] = randomNumber;
+            for (int i = 0; i < size; i++) {
+                int randomNumber = rand() % 10001;
+                arr1[i] = randomNumber;
+                arr2[i] = randomNumber;
+            }
+
+            clock_t t = clock();
+            insertionSort(arr1, size);
+            t = clock() - t;
+            insertionSortTime += ((double)t) / CLOCKS_PER_SEC;
+
+            clock_t t2 = clock();
+            mergeSort(arr2, 0, size - 1);
+            t2 = clock() - t2;
+            mergeSortTime += ((double)t2) / CLOCKS_PER_SEC;
+
+            free(arr1);
+            free(arr2);
+
+            printf("Execução %d:\n", nExecucoes + 1);
+            printf(" - InsertionSort: %.6f s\n", ((double)t) / CLOCKS_PER_SEC);
+            printf(" - MergeSort: %.6f s\n", ((double)t2) / CLOCKS_PER_SEC);
         }
 
-        //printf("Array original: ");
-        //printArray(arr1, size);
-
-        t = clock();
-        insertionSort(arr1, size);
-        t = clock() - t;
-
-        //printf("Array ordenado: ");
-        //printArray(arr1, size);
-
-        //printf("Array original: ");
-        //printArray(arr2, size);
-
-        t2 = clock();
-        mergeSort(arr2, 0, size - 1);
-        t2 = clock() - t2;
-
-        //printf("Array ordenado: ");
-        //printArray(arr2, size);
-
-        printf("EXECUÇÃO %i\n", nExecucoes + 1);
-        printf("Tempo de execucao do InsertionSort: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
-        printf("Tempo de execucao do MergeSort: %lf ms\n\n", ((double)t2)/((CLOCKS_PER_SEC/1000)));
+        printf("\nMédia dos tempos:\n");
+        printf(" - InsertionSort: %.6f s\n", insertionSortTime / 10.0);
+        printf(" - MergeSort: %.6f s\n\n", mergeSortTime / 10.0);
     }
 
+    return 0;
 }
