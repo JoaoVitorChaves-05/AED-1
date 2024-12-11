999ba9e21893d1b932d272c27b1048a3056b1bdf
João Vitor Mancio Chaves <80546427+JoaoVitorChaves-05@users.noreply.github.com>
2024-12-11 01:08:44 +0000

    file

--- atividades/insertion_merge_e_quick_sort.c
+++ atividades/insertion_merge_e_quick_sort.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
@@ -13,18 +42,6 @@ void insertionSort(int arr[], int n) {
         }
 
         arr[j + 1] = key;
-    }
-}
-
-void mergeSort(int arr[], int left, int right);
-void merge(int arr[], int left, int mid, int right);
-
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
     }
 }
 
@@ -57,21 +74,43 @@ void merge(int arr[], int left, int mid, int right) {
     free(R);
 }
 
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main() {
-    for (int size = 50000; size <= 500000; size += 50000) {
+    for (int size = 20000; size <= 400000; size += 20000) {
         printf("\nPara N = %d:\n", size);
         double insertionSortTime = 0;
         double mergeSortTime = 0;
+        double quickSortTime = 0;
 
-        for (int nExecucoes = 0; nExecucoes < 3; nExecucoes++) {
+        float qtdExecucoes = 3.0;
+
+        for (int nExecucoes = 0; nExecucoes < qtdExecucoes; nExecucoes++) {
             int *arr1 = malloc(size * sizeof(int));
             int *arr2 = malloc(size * sizeof(int));
-            srand(time(NULL));
+            int *arr3 = malloc(size * sizeof(int));
+
+            srand(time(NULL) + nExecucoes);
 
             for (int i = 0; i < size; i++) {
                 int randomNumber = rand() % 10001;
                 arr1[i] = randomNumber;
                 arr2[i] = randomNumber;
+                arr3[i] = randomNumber;
             }
 
             clock_t t = clock();
@@ -84,17 +123,25 @@ int main() {
             t2 = clock() - t2;
             mergeSortTime += ((double)t2) / CLOCKS_PER_SEC;
 
+            clock_t t3 = clock();
+            quickSort(arr3, 0, size - 1);
+            t3 = clock() - t3;
+            quickSortTime += ((double)t3) / CLOCKS_PER_SEC;
+
             free(arr1);
             free(arr2);
+            free(arr3);
 
             printf("Execução %d:\n", nExecucoes + 1);
             printf(" - InsertionSort: %.6f s\n", ((double)t) / CLOCKS_PER_SEC);
             printf(" - MergeSort: %.6f s\n", ((double)t2) / CLOCKS_PER_SEC);
+            printf(" - QuickSort: %.6f s\n", ((double)t3) / CLOCKS_PER_SEC);
         }
 
         printf("\nMédia dos tempos:\n");
-        printf(" - InsertionSort: %.6f s\n", insertionSortTime / 3);
-        printf(" - MergeSort: %.6f s\n\n", mergeSortTime / 3);
+        printf(" - InsertionSort: %.6f s\n", insertionSortTime / qtdExecucoes);
+        printf(" - MergeSort: %.6f s\n", mergeSortTime / qtdExecucoes);
+        printf(" - QuickSort: %.6f s\n", quickSortTime / qtdExecucoes);
     }
 
     return 0;
