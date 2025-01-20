b14ada009dfe09f6baffa2cecdb4a4642196bca5
João Vitor Mancio Chaves <80546427+JoaoVitorChaves-05@users.noreply.github.com>
2025-01-20 21:30:48 +0000

    1068

--- atividades/1068.c
+++ atividades/1068.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-const int MAX_N = 5;
+#define MAX_LENGTH 1000
+
+int isBalanced(const char* expression) {
+    int balance = 0;
+    for (int i = 0; expression[i] != '\0'; i++) {
+        if (expression[i] == '(') {
+            balance++;
+        } else if (expression[i] == ')') {
+            balance--;
+            if (balance < 0) {
+                return 0;
+            }
+        }
+    }
+    return balance == 0;
+}
 
 int main() {
-    int N = 0;
-    char out[MAX_N][10];
-
-    while (N < MAX_N) {
-        char math[10000];
-        //if (fgets(math, sizeof(math), stdin) == NULL) {
-            //break;
-        //}
-
-        //math[strcspn(math, "\n")] = '\0';
-
-        scanf("%s", &math); 
-
-        char pilha[10000];
-        int i = 0;
-        int j = 0;
-
-        while (math[i] != '\0') {
-            if (math[i] == '(' || math[i] == ')') {
-                if (j == 0) {
-                    pilha[j] = math[i];
-                    j++;
-                } else if (pilha[j-1] == '(' && math[i] == ')') {
-                    j--;
-                } else if (math[i] == '(' || math[i] == ')') {
-                    pilha[j] = math[i];
-                    j++;
-                }
-            }
-            i++;
-        }
-
-        if (j == 0)
-            strcpy(out[N], "correct");
-        else
-            strcpy(out[N], "incorrect");
-
-        N++;
-    }
+    char expression[MAX_LENGTH + 1];
     
-    for (int k = 0; k < MAX_N; k++)
-        printf("%s\n", out[k]);
+    while (fgets(expression, sizeof(expression), stdin)) {
+        expression[strcspn(expression, "\n")] = '\0';
+        
+        if (isBalanced(expression)) {
+            printf("correct\n");
+        } else {
+            printf("incorrect\n");
+        }
+    }
+
+    return 0;
 }
