977152697425464790d2c06054d80fa87c4062b8
João Vitor Chaves <joaovmanciochaves@gmail.com>
2025-02-09 22:06:53 -0300

    feat: exercícios

--- apostila/grafos/aula61.c
+++ apostila/grafos/aula61.c
@@ -87,15 +87,48 @@ int adicionaAresta(Graph *G, int origem, int destino, int eh_digrafo, float peso
         G->pesos[origem][G->grau[origem]] = peso;
     G->grau[origem]++;  // Incrementa corretamente o grau do vértice
 
-    if (!eh_digrafo)
-        adicionaAresta(G, destino, origem, 1, peso); // Adiciona a aresta inversa
+    if (eh_digrafo == 1)
+        adicionaAresta(G, destino, origem, 0, peso); // Adiciona a aresta inversa
+
+    return 1;
+}
+
+int removeAresta(Graph *G, int origem, int destino, int eh_digrafo) {
+    if (G == NULL)
+        return 0;
+    if (origem < 0 || origem >= G->numVertices || destino < 0 || destino >= G->numVertices)
+        return 0;
+
+    int i = 0;
+    while (i < G->grau[origem] && G->arestas[origem][i]!= destino)
+        i++;
+    
+    if (i == G->grau[origem])
+        return 0;
+
+
+    for (int j = i; j < G->grau[origem] - 1; j++) {
+        G->arestas[origem][j] = G->arestas[origem][j+1];
+        if (G->ehPonderado)
+            G->pesos[origem][j] = G->pesos[origem][j+1];  // Remove peso
+    }
+    
+    G->grau[origem]--;  // Decrementa corretamente o grau do vértice
+
+    // Remove a aresta inversa (se for digrafo)
+    if (eh_digrafo == 1) {
+        removeAresta(G, destino, origem, 0);
+    }
 
     return 1;
 }
 
 
 int main() {
-    gr = criaGrafo(10, 7, 0); // (numVertices, grau_max, grau)
-    adicionaAresta(gr, 0, 1, 0, 1);
-    adicionaAresta(gr, 0, 2, 0, 2);
+    Graph* gr = criaGrafo(10, 7, 0); // (numVertices, grau_max, grau)
+    adicionaAresta(gr, 0, 1, 0, 1); // 0 -> 1
+    adicionaAresta(gr, 0, 2, 0, 2); // 0 -> 2
+    adicionaAresta(gr, 1, 2, 0, 3); // 1 -> 2
+    adicionaAresta(gr, 2, 3, 0, 4); // 2 -> 3
+    adicionaAresta(gr, 2, 4, 0, 5); // 2 -> 4
 }
